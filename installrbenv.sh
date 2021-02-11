7eaa46214eabf877cb822e826f237d23bf44f9e3
Emmanuel Cohen-Laroque <9961473+ecl66@users.noreply.github.com>
2021-02-11 13:11:42 +0100

    Update installrbenv.sh

--- installrbenv.sh
+++ installrbenv.sh
@@ -1,37 +1,26 @@
 #
 #!/usr/bin/bash
-
+#
 sudo apt install curl
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-
 sudo apt-get update
 sudo apt-get install git-core zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev nodejs yarn
-
-
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-
-POSTGRESQL
+# POSTGRESQL
 sudo apt install libpq-dev
-
-NODE.JS
+# NODE.JS
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
-
-ADD YARN FOR WEBPACKER:
-
+# ADD YARN FOR WEBPACKER:
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
-
-
-NO DOC: in .gemrc
-gem: --no-document
+# NO DOC: in .gemrc
+#gem: --no-document
